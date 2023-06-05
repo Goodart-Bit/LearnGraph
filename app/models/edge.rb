b4b12d7c452013668d84c40c0cf62f815b48fe89
Goodart-Bit <juandlh416@gmail.com>
2023-06-05 18:53:49 -0500

    Added notes, edges and user associations and validations

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,5 +1,10 @@
 class Edge < ApplicationRecord
-  belongs_to :note
-  belongs_to :note
-  belongs_to :user
+  belongs_to :source, class_name: 'Note'
+  belongs_to :target, class_name: 'Note'
+  validate :non_recursive_association
+
+  def non_recursive_association
+    errors.add(:target_id, 'no puede ser igual a la nota actual') if
+      source_id == target_id
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
-  validates_associated :user
-  validates :title, presence: true
-  validates :title, uniqueness: true
+  has_many :pointers, class_name: 'Edge', foreign_key: :source_id
+  has_many :mentions, class_name: 'Edge', foreign_key: :target_id
+
+  validates :title, presence: true, uniqueness: true,
+            length: { in: 5..255, message: 'debe tener más de 5 carácteres' }
 end
