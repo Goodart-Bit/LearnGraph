32e886fa0b54c9e1af7ecb07193d338041285005
Goodart-Bit <juandlh416@gmail.com>
2023-06-05 20:27:48 -0500

    Modified notes create and index method to: 1. Get user only notes 2. Display flash messages (only on create)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,9 +7,11 @@ class NotesController < ApplicationController
         note_data = params[:note]
         @note = Note.new(title: note_data[:title], body: note_data[:body], user_id: current_user.id)
         if @note.save
-            puts "saved succesfully"
+            flash[:alert] = 'Nota guardada con éxito' # TODO: Fish flash alert not showing on redirect
+            redirect_to edit_note_path(@note), status: :see_other
         else
-            p @note.errors
+            flash[:alert] = 'Hubo un error al intentar guardar la nota'
+            render action: :new, status: :unprocessable_entity
         end
     end
 
@@ -19,7 +21,7 @@ class NotesController < ApplicationController
     end
 
     def index
-        @notes = Note.all
+        @notes = current_user.notes
     end
 
     def show
