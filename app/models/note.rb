d3805e89e6e772c1af34981c9b5ad146204e4842
Goodart-Bit <juandlh416@gmail.com>
2023-07-01 17:53:50 -0500

    Added dependent destroy specification to note.rb for note edges

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
   belongs_to :user
-  has_many :pointers, class_name: 'Edge', foreign_key: :source_id
-  has_many :mentions, class_name: 'Edge', foreign_key: :target_id
+  has_many :pointers, class_name: 'Edge', foreign_key: :source_id, dependent: :destroy
+  has_many :mentions, class_name: 'Edge', foreign_key: :target_id, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true,
             length: { in: 5..255, message: 'debe tener más de 5 carácteres' }
