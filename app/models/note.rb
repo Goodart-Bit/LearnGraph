aaccb4bc0a693de8358b884b68485a4b6e48330a
Goodart-Bit <juandlh416@gmail.com>
2023-07-02 12:23:16 -0500

    Added TODO for certain notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,6 @@ class Note < ApplicationRecord
   has_many :pointers, class_name: 'Edge', foreign_key: :source_id, dependent: :destroy
   has_many :mentions, class_name: 'Edge', foreign_key: :target_id, dependent: :destroy
 
-  validates :title, presence: true, uniqueness: true,
+  validates :title, presence: true, uniqueness: true, #TODO: Make uniqueness only apply for notes' titles of the same user
             length: { in: 5..255, message: 'debe tener más de 5 carácteres' }
 end
