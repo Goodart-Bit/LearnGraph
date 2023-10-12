6c988ad06aa2bea13e4dad5adbc0927aa9eb467f
Goodart-Bit <juandlh416@gmail.com>
2023-10-11 19:03:34 -0500

    Added unit tests for users, notes and edges

--- test/models/edge_test.rb
+++ test/models/edge_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class EdgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def before_setup
+    super
+    @user_note = Note.find 990
+    @other_note = Note.find 991
+    @edge = Edge.create(source_id: @user_note.id, target_id: @other_note.id)
+  end
+
+  test "can create an edge" do
+    assert Edge.find @edge.id
+  end
+
+  test "can delete an edge" do
+    @edge.destroy
+    assert_raises ActiveRecord::RecordNotFound do
+      Edge.find @edge.id
+    end
+  end
+
+  test "created edge is reflected on both notes" do
+    points_to_other = @user_note.pointers.include? @edge
+    other_has_mention = @other_note.mentions.include? @edge
+    assert points_to_other && other_has_mention
+  end
+
+  test "deleted edge is reflected on both notes" do
+    @edge.destroy
+    points_to_other = @user_note.pointers.include? @edge
+    other_has_mention = @other_note.mentions.include? @edge
+    assert_not points_to_other || other_has_mention
+  end
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def before_setup
+    super
+    @user = User.find(999)
+    @note = Note.create( user: @user, title: "Prueba")
+  end
+
+  test "user can create note" do
+    assert Note.find( @note.id )
+  end
+
+  test "user cannot create note without title" do
+    titleless_note = Note.new( user: @user, body: "Something")
+    assert_not  titleless_note.valid?
+  end
+
+  test "user cannot create notes with duplicate titles" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Note.create!( user: @user, title: @note.title )
+    end
+  end
+
+  # TODO: Fix model constraints
+  test "other users can create notes with the same title" do
+    other_user = User.find(998)
+    other_note = Note.create( user: other_user, title: @note.title )
+    assert other_note.valid?
+  end
+
+  test "note can be deleted" do
+    @note.destroy
+    assert_raises ActiveRecord::RecordNotFound do
+      Note.find(@note.id)
+    end
+  end
+
+  test "can update note title" do
+    updated_title = "New title"
+    @note.update( title: updated_title )
+    assert @note.title === updated_title
+  end
+
+  test "can update note" do
+    original_title = @note.title
+    original_body = @note.body
+    @note.update( title: "#{original_title} new", body: "#{original_body} new" )
+    assert @note.title != original_title && @note.body != original_body
+  end
 end
